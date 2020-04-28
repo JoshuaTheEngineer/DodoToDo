8738ef8956b3bb645ea4164059ede9f1ade84beb
Joshua Cadavez <cadav001@umn.edu>
2020-04-28 02:29:11 -0400

    DataBinding successfully implemented as seen with fab btn

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # DodoToDo
 
-## Setup
+## Step 1: Setup
+
+View Tag ***v1.0.0-Setup***
 
 1. Add a Vector Asset based on Android's preexisting Add, Edit, Delete, and Check icons 0. To allow custom assets, - go to build.gradle(app) - set this 'vectorDrawables.useSupportLibrary = true' under 'defaultConfig'
     i. Right Click the app directory and choose 'Create New Vector Asset*
@@ -12,3 +14,11 @@
 2. Add [LifeCycle and Room dependencies](https://developer.android.com/topic/libraries/architecture/adding-components). This will include support for:
     - LiveData
     - ViewModel
+
+## Step 2: Notes List UI
+
+1. Add a RecyclerView under [DataBinding](https://developer.android.com/topic/libraries/data-binding)
+
+    - Optional. Use ButterKnife. In "File > Project Structure > Dependencies > App", click the add button to include latest versions of
+        i. jakewharton:butterknife
+        ii. jakewharton:butterknife-compiler (added as annotationProcessor)
--- app/src/main/java/com/joshuatheengineer/dodotodo/MainActivity.java
+++ app/src/main/java/com/joshuatheengineer/dodotodo/MainActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.joshuatheengineer.dodotodo.databinding.ActivityMainBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 
 import android.view.View;
 import android.view.Menu;
@@ -14,15 +16,19 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // declare binding
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // sets DataBinding to the content view
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        // example of using DataBinding with the fab btn
+        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
